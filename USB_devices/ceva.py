ef23c00afbaaeac069bda76a9812a461870e5979
George-Cristian Muraru <george.muraru@padawan.devacademy.ro>
2015-03-08 19:20:31 +0200

    remove

--- USB_devices/ceva.py
+++
@@ -1,8 +0,0 @@
-import usb
-for dev in usb.core.find(find_all=True):
-	print dev
-	import pdb; pdb.set_trace()
-#	print "Manufacturer:", dev.manufacturer
-#	print "Vendor:", dev.iProduct
-	print "  idVendor: %d (%s)" % (dev.idVendor, hex(dev.idVendor))
-	print "  idProduct: %d (%s)" % (dev.idProduct, hex(dev.idProduct))
