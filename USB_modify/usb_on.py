df6cc44215a72e9732eba367c15ee4771fc19559
George-Cristian <murarugeorgec@gmail.com>
2015-06-24 04:50:19 +0300

    Modified print for python3

--- USB_modify/usb_on.py
+++ USB_modify/usb_on.py
@@ -7,11 +7,11 @@ def usb_enable(bus_id):
 		with open("/sys/bus/usb/drivers/usb/bind", "w") as bind_device:
 			bind_device.write(bus_id)
 	except IOError:
-		print "Check if the bus_id exists"
+		print("Check if the bus_id exists")
 
 if __name__ == "__main__":
 	try:
 		usb = sys.argv[1]
 		usb_enable(usb)
 	except IndexError:
-		print "Must have as argument the usb port!"
+		print("Must have as argument the usb port!")
