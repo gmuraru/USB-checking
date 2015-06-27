9fe52ebb3cf7d53e07a6ebeebffe5e3d877993ae
George-Cristian <murarugeorgec@gmail.com>
2015-06-28 02:51:05 +0300

    Added bus_id for already connected devices

--- USB_modify/usb-inhibit.py
+++ USB_modify/usb-inhibit.py
@@ -18,6 +18,7 @@ monitor.filter_by(subsystem='usb')
 FLAG_REMAIN_UNSEEN = False
 
 
+# The proper way to use the usb-inhibit
 def usage():
 	print ("Bad program call"
 			"\nCall:"
@@ -29,34 +30,45 @@ def usage():
 			"\n\t\tthe program execution) unloaded")
 
 
-
+# Look for already connected devices and take their bus_id
 def form_initial_devices():
-	already_connected = []
-	for full_dev in glob.glob(files_to_look):
-		already_connected.append(full_dev.split("/")[-1])
+
+	for device in context.list_devices(subsystem='usb'):
+
+		if device.device_type == 'usb_device' and \
+			device.find_parent(subsystem='usb', device_type='usb_device') != None:
+
+			already_connected.append(device.sys_name.split('/')[-1])
+
+
 	return already_connected
 
 def start_process(process_with_args):
-	pid = subprocess.Popen(process_with_args)
+
+	try:
+		pid = subprocess.Popen(process_with_args)
+	except OSError:
+		pid = None
+
 	return pid
 
 def rebind_devices():
 	
-	with open("/sys/bus/usb/drivers_autoprobe", "wt") as f_out:
+	with open('/sys/bus/usb/drivers_autoprobe', 'wt') as f_out:
 		f_out.write("1")
 
 	if not FLAG_REMAIN_UNSEEN:
 		for dev in not_connected:
-			usb_on.usb_enable(dev.split("/")[-1])
+			usb_on.usb_enable(dev.split('/')[-1])
 
 def inhibit_USB(pid):
 
 	monitor.start()
-	with open("/sys/bus/usb/drivers_autoprobe", "wt") as f_out:
+	with open('/sys/bus/usb/drivers_autoprobe', 'wt') as f_out:
 		f_out.write("0")
 
 	for action, device in monitor:
-		dev = device.sys_name.split(":")[0]
+		dev = device.sys_name.split(':')[0]
 		print(dev)
 		if action == 'add':
 			not_connected.append(dev)
@@ -67,16 +79,23 @@ def inhibit_USB(pid):
 if __name__ == "__main__":
 
 	process_with_args = sys.argv[2:]
+	
 	if not process_with_args:
 		usage()
+
 	else:
 		pid = start_process(process_with_args)
+		
+		if pid == None:
+			print ("Enter a valid command!")
 
-		already_connected = form_initial_devices()
+		else:		
+		
+			already_connected = form_initial_devices()
 
-		try:
-			inhibit_USB(pid)
-			rebind_devices()
-		except KeyboardInterrupt:
-			rebind_devices()
+			try:
+				inhibit_USB(pid)
+				rebind_devices()
+			except KeyboardInterrupt:
+				rebind_devices()
 
