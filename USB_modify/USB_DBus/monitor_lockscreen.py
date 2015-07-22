c542f06bff8b43693e64629fe39817e3d5737040
George-Cristian Muraru <murarugeorgec@gmail.com>
2015-07-22 12:12:05 +0300

    renamed variables

--- USB_modify/USB_DBus/monitor_lockscreen.py
+++ USB_modify/USB_DBus/monitor_lockscreen.py
@@ -9,7 +9,6 @@ bus2 = dbus.SystemBus()
 usb_service_proxy = bus2.get_object('org.gnome.USBBlocker', '/org/gnome/USBBlocker')
 
 def notificationScreen(bus, message):
-	print(message)
 	if message.get_member() != "ActiveChanged":
 		return
 
@@ -17,17 +16,16 @@ def notificationScreen(bus, message):
 	args = message.get_args_list()
 
 	# Check if the screen is locked or unlocked
-	print (args)
-
+	print (args[0])
 	if args[0] == True:
 		print("Screen Locked")
-		usb_service.get_dbus_method('start_monitor', 'org.gnome.USBBlocker.monitor')()
+		usb_service_proxy.get_dbus_method('start_monitor', 'org.gnome.USBBlocker.monitor')()
+		print("Reach")
 	else:
 		print("Screen Unlocked")
-		usb_service.get_dbus_method('stop_monitor', 'org.gnome.USBBlocker.monitor')()
+		usb_service_proxy.get_dbus_method('stop_monitor', 'org.gnome.USBBlocker.monitor')()
 
 DBusGMainLoop(set_as_default=True)
-
 
 bus = dbus.SessionBus()
 bus.add_match_string("type='signal', interface='org.gnome.ScreenSaver'")
